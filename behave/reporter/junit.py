a49da8afa5d638b9d26c81329bc73a8f29e8eca1
Maxim Nikolaev <m-nikolaev@igrade.ru>
2014-03-17 18:10:02 +0400

    Junit message encoding exception fixed

--- behave/reporter/junit.py
+++ behave/reporter/junit.py
@@ -7,7 +7,7 @@ from behave.reporter.base import Reporter
 from behave.model import Scenario, ScenarioOutline, Step
 from behave.formatter import ansi_escapes
 from behave.model_describe import ModelDescriptor
-from behave.textutil import indent, make_indentation
+from behave.textutil import indent, make_indentation, ensure_unicode
 
 
 def CDATA(text=None):
@@ -236,7 +236,7 @@ class JUnitReporter(Reporter):
             failure = ElementTree.Element(element_name)
             step_text = self.describe_step(step).rstrip()
             text = u"\nFailing step: %s\nLocation: %s\n" % (step_text, step.location)
-            message = unicode(step.exception)
+            message = ensure_unicode(step.exception)
             if len(message) > 80:
                 message = message[:80] + "..."
             failure.set('type', step.exception.__class__.__name__)
